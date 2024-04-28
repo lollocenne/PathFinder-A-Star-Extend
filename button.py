8e712d6544777a7286c921efbc2e7f236dfd296d
lollocenne <lorenzo.cennerilli@gmail.com>
2024-04-28 13:32:39 +0200

    Added buttons

--- button.py
+++ button.py
@@ -2,34 +2,41 @@ import pygame
 
 
 class Button():
-    def __init__(self, window, x: float, y: float, width: float, height: float, text: str = "", color: tuple = (50, 50, 50), textColor: tuple = (255, 255, 255)):
+    def __init__(self, window: pygame.Surface, x: float, text: str = ""):
         self.WINDOW = window
         self.WINDOW_WIDTH: int = window.get_width()
         self.WINDOW_HEIGHT: int = window.get_height()
-
+        
         self.x = x
-        self.y = y
-
-        self.width = width
-        self.height = height
-
+        self.y = 0
+        
+        self.width = window.get_width()/8
+        self.height = 20
+        
         self.string = text
-
-        self.color = color
-        self.textColor = textColor
-
-        self.FONT = pygame.font.SysFont("comicsan", 25)
+        
+        self.color = (50, 50, 50)
+        self.colorActive = (70, 70, 70)
+        self.textColor = (255, 255, 255)
+        
+        self.FONT = pygame.font.SysFont("comicsan", 20)
         self.text = self.FONT.render(self.string, True, self.textColor)
-
-        self.hitbox = pygame.Rect(self.x - self.width/2, self.y - self.height/2, self.width, self.height)
-
+        
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+        
+        self.active = False
+    
     def press(self) -> bool:
         if pygame.mouse.get_pressed()[0]:
             mouse = pygame.mouse.get_pos()
-
+            
             if self.hitbox.collidepoint(mouse[0], mouse[1]):
                 return True
     
     def draw(self) -> None:
-        pygame.draw.rect(self.WINDOW, self.color, self.hitbox)
-        self.WINDOW.blit(self.text, (self.x - self.text.get_width()/2, self.y - self.text.get_height()/2))
+        if self.active:
+            pygame.draw.rect(self.WINDOW, self.colorActive, self.hitbox)
+        else:
+            pygame.draw.rect(self.WINDOW, self.color, self.hitbox)
+        
+        self.WINDOW.blit(self.text, (self.x + self.width/2 - self.text.get_width()/2, self.y + self.text.get_height()/2))
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
 from button import Button
 
+pygame.font.init()
 
 WIDTH, HEIGHT = 820, 570
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PathFinder A*")
 
+buttonsText = ("Start", "End", "Start Node", "End Node", "Node", "Road", "Grab", "Delete")
+buttons: list[Button] = []
+
+for i in range(len(buttonsText)):
+    buttons.append(Button(WINDOW, i*WIDTH/8 + i, buttonsText[i]))
+
+buttons[0].active = True
 
 def update():
     pass
 
 def draw():
-    pass
+    for b in buttons:
+        b.draw()
     
     pygame.display.update()
 
